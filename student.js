043cb31cc646650c2eb57c37268a36104b0be9bd
Darshan <darshanchauhan5064@gmail.com>
2024-01-23 09:51:23 +0530

    23 jan

---
+++ student.js
@@ -0,0 +1,82 @@
+db.client_masters.insertMany([
+    {
+        StudentNo: '1',
+        Name: 'vijay solanki',
+        Address1 : 'E-79',
+        Address2 : 'matrushakti',
+        City: 'surat',
+        Pincode: '395010',
+        State: 'Gujrat',
+        BalDue: 15000
+    },
+    {
+        StudentNo: '2',
+        Name: 'Dhaval solanki',
+        Address1 : 'B2',
+        Address2 : 'Chennai',
+        City: 'Madras',
+        Pincode: '780001',
+        State: 'Tamil Nadu',
+        BalDue: 2000
+    },
+    {
+        StudentNo: '3',
+        Name: 'Darshan chauhan',
+        Address1 : '209',
+        Address2 : 'Ravipark',
+        City: 'Surat',
+        Pincode: '400057',
+        State: 'Gujarat',
+        BalDue: 5000
+    },
+    {
+        StudentNo: '4',
+        Name: 'Jenil kakadiya',
+        Address1 : 'A-28',
+        Address2 : 'MS',
+        City: 'Bangalore',
+        Pincode: '560001',
+        State: 'Karnataka',
+        BalDue: 0
+    },
+    {
+        StudentNo: '5',
+        Name: 'krish rangani',
+        Address1 : 'P-7',
+        Address2 : 'Bandra',
+        City: 'Mumbai',
+        Pincode: '400060',
+        State: 'Maharashtra',
+        BalDue: 2000
+    },
+    {
+        StudentNo: '6',
+        Name: 'Vijay dankhra',
+        Address1 : 'L9',
+        Address2 : 'Long Street',
+        City: 'Mangalore',
+        Pincode: '560050',
+        State: 'Karnataka',
+        BalDue: 0
+    },
+    {
+        StudentNo: '7',
+        Name: 'moni kathiriya',
+        Address1 : '70',
+        Address2 : 'Long Street',
+        City: 'Mangalore',
+        Pincode: '560050',
+        State: 'Karnataka',
+        BalDue: 0
+    },
+    {
+        StudentNo: '7',
+        Name: 'moni kathiriya',
+        Address1 : '70',
+        Address2 : 'Long Street',
+        City: 'Mangalore',
+        Pincode: '560050',
+        State: 'Karnataka',
+        BalDue: 0
+    },
+]);
